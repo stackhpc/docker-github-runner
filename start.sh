1eb84a75a75c0d152a96a5c249a8ae95cbd79620
Piotr Parczewski <piotr@stackhpc.com>
2021-05-20 17:16:46 +0200

    Initial commit

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+ORGANIZATION=$ORGANIZATION
+ACCESS_TOKEN=$ACCESS_TOKEN
+REPOSITORY=$REPOSITORY
+
+REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${ORGANIZATION}/$REPOSITORY/actions/runners/registration-token | jq .token --raw-output)
+
+cd /home/docker/actions-runner
+
+./config.sh --url https://github.com/${ORGANIZATION}/$REPOSITORY --token ${REG_TOKEN}
+
+cleanup() {
+            echo "Removing runner..."
+                ./config.sh remove --unattended --token ${REG_TOKEN}
+        }
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+./run.sh & wait $!
